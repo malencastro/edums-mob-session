f026e92a299e914dc5732d905e1862bdda114382
Mariana Alencastro <mariana.alencastro@valtech.com>
2023-12-13 17:17:31 +0000

    Create blank project

---
+++ EduMSMobSession/EduMSMobSession/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EduMSMobSession
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
